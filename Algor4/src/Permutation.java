dc7d9417f25ba937898060c605508057a3eaed9f
Anderbone <yanchiyu@outlook.com>
2017-10-25 22:17:48 +0800

    try again

--- Algor4/src/Permutation.java
+++ Algor4/src/Permutation.java
@@ -6,23 +6,23 @@ public class Permutation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RandomizedQueue<String> s=new RandomizedQueue<String>();
-		/*int k=Integer.parseInt(args[0]);
+		int k=Integer.parseInt(args[0]);
 		
 		String str;
 		while(!StdIn.isEmpty()){
 		str=StdIn.readString();
 		s.enqueue(str);
 		}
-		*/
 		
 		
+		/*
 		int k=3;
 		s.enqueue("d");
 		s.enqueue("g");
 		s.enqueue("a");
 		s.enqueue("c");
 		s.enqueue("j");
-		
+		*/
 		for(int i=0;i<k;i++){
 			StdOut.print(s.dequeue());
 		}
